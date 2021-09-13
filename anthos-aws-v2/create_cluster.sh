013073a43b3966c7b84505ef8789b5f4adbfe4f8
Rick Chen <rickruguichen@google.com>
2021-09-13 20:33:34 +0000

    cleanup create cluster

--- anthos-aws-v2/create_cluster.sh
+++ anthos-aws-v2/create_cluster.sh
@@ -6,11 +6,6 @@ export CLUSTER_NAME=anthos-aws-v2
 export OUTPUT_FILE=aws-resources.sh
 source $OUTPUT_FILE
 
-PROJECT_ID="$(gcloud config get-value project)"
-gcloud services enable gkeconnect.googleapis.com 
-MEMBER="serviceAccount:$PROJECT_ID.svc.id.goog[gke-system/gke-multicloud-agent]"
-gcloud projects add-iam-policy-binding "$PROJECT_ID" \
-    --member="$MEMBER"  --role="roles/gkehub.connect"
 
 gcloud alpha container aws clusters create $CLUSTER_NAME \
   --aws-region=$AWS_REGION --location=$GCP_REGION \
@@ -26,5 +21,10 @@ gcloud alpha container aws clusters create $CLUSTER_NAME \
   --ssh-ec2-key-pair=$SSH_KEY_PAIR_NAME \
   --main-volume-size=10 \
   --root-volume-size=10 \
-  --role-arn=$API_ROLE_ARN 
-
+  --role-arn=$API_ROLE_ARN \
+  --role-session-name=ROLE_SESSION_NAME
+PROJECT_ID="$(gcloud config get-value project)"
+gcloud services enable gkeconnect.googleapis.com MEMBER="serviceAccount:$PROJECT_ID.svc.id.goog[gke-system/gke-multicloud-agent]"
+gcloud projects add-iam-policy-binding "$PROJECT_ID" \
+    --member="$MEMBER" \
+    --role="roles/gkehub.connect"
