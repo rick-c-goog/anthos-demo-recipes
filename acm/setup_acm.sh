50ce004015861702c4d63db48256c734280b47a6
Rick Chen <rickruguichen@google.com>
2021-09-14 02:51:20 +0000

    acm setup script

--- acm/setup_acm.sh
+++ acm/setup_acm.sh
@@ -1,13 +1,20 @@
-keyfile=/pathToKeyFileName
+keyfile=acm-key
+
+ssh-keygen -t rsa -b 4096 \
+-C "rick-c-goog" \
+-N '' \
+-f $keyfile
+
+gcloud container hub memberships get-credentials $CLUSTER_NAME
 kubectl create ns config-management-system && \
 kubectl create secret generic git-creds \
  --namespace=config-management-system \
  --from-file=ssh=$keyfile
 
-PROJECT_ID=$(gcloud config get value get-project))
+PROJECT_ID=$(gcloud config get-value project)
 gcloud beta container hub config-management apply \
       --membership=anthos-aws-v2 \
-      --config=$HOME/anthos-aws-v2/acm/apply-spec.yaml \
+      --config=apply-spec.yaml \
       --project=$PROJECT_ID
 
 
