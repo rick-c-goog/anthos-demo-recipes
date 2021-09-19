7738053e96615b99b8cd83d520b083bb20512eed
Rick(Rugui) Chen <rickruguichen@google.com>
2021-09-19 12:40:41 -0400

    issuer

--- attached_eks/eks/eks_kubeconfig.sh
+++ attached_eks/eks/eks_kubeconfig.sh
@@ -26,6 +26,17 @@ else
   aws-iam-authenticator version
 fi
 
+export EKSCTL_INSTALLED=`which eksctl`
+if [[ ${EKSCTL_INSTALLED} ]]; then
+  title_no_wait "eksctl is already installed."
+else
+  #curl -o $HOME/.local/bin/eksctl https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/aws-iam-authenticator
+  curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C .
+  mv eksctl /$HOME/.local/bin/
+  chmod +x /$HOME/.local/bin/eksctl
+  #aws-iam-authenticator version
+fi
+
 # Create KSA and token
 kubectl --kubeconfig=kubeconfig_$EKS apply -f - <<EOF
 apiVersion: v1
@@ -50,7 +61,7 @@ EOF
 
 SECRET_NAME=$(kubectl --kubeconfig=kubeconfig_$EKS get serviceaccount gke-hub-ksa -o jsonpath='{$.secrets[0].name}')
 kubectl --kubeconfig=kubeconfig_$EKS get secret ${SECRET_NAME} -o jsonpath='{$.data.token}' | base64 -d > $EKS-ksa-token.txt
-
+eksctl utils associate-iam-oidc-provider --cluster $EKS --approve
 # Upload the ksa-token to GCS bucket
 gsutil cp -r $EKS-ksa-token.txt gs://$PROJECT_ID/kubeconfig/$EKS-ksa-token.txt
 
@@ -58,7 +69,7 @@ gsutil cp -r $EKS-ksa-token.txt gs://$PROJECT_ID/kubeconfig/$EKS-ksa-token.txt
 gsutil cp -r gs://${PROJECT_ID}/hubgsa/gke_hub_sa_key.json .
 
 # Register the EKS cluster
-gcloud container hub memberships register ${EKS} --project=${PROJECT_ID} --context=eks_${EKS} --kubeconfig=kubeconfig_${EKS} --service-account-key-file=gke_hub_sa_key.json
-
+#gcloud container hub memberships register ${EKS} --project=${PROJECT_ID} --context=eks_${EKS} --kubeconfig=kubeconfig_${EKS} --service-account-key-file=gke_hub_sa_key.json
+gcloud container hub memberships register ${EKS} --project=${PROJECT_ID} --context=eks_${EKS} --kubeconfig=kubeconfig_${EKS}  --enable-workload-identity   --public-issuer-url=${OIDC_URL}
 # Add labels to the registered cluster
 gcloud container hub memberships update ${EKS} --update-labels environ=${ENV},infra=aws
